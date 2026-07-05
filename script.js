c353c4a06fa4cee7a8d6250448854a0c7be588d3
Mark Joshua Sanchez <markjoshuasanchez23@gmail.com>
2026-07-05 14:15:32 +0800

    Fixed the bug on Mobile View

--- script.js
+++ script.js
@@ -90,12 +90,6 @@ const observer = new IntersectionObserver((entries) => {
     threshold: 0.2
 });
 
-cards.forEach(card => observer.observe(card));
-const mobileOpen = document.getElementById("mobileOpenModal");
-
-if (mobileOpen) {
-    mobileOpen.addEventListener("click", (e) => {
-        e.stopPropagation();
-        modal.classList.add("active");
-    });
-}
+cards.forEach(card => {
+    observer.observe(card);
+});
