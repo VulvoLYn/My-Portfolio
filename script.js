112ec5829cef9c4ea63d0ae8799b0d55d38ddcd3
Mark Joshua Sanchez <markjoshuasanchez23@gmail.com>
2026-07-05 11:09:52 +0800

    Updated version V.1

--- script.js
+++ script.js
@@ -67,3 +67,29 @@ window.onclick = (e) => {
         modal.classList.remove("active");
     }
 };
+
+/* ==========================
+   MOBILE SCROLL ANIMATION
+========================== */
+
+const cards = document.querySelectorAll(".mobile-card");
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+}, {
+
+    threshold: 0.2
+
+});
+
+cards.forEach(card => observer.observe(card));
